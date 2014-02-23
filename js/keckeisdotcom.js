34c52ea5c03e7974db2baf9a842df100b2cab299
nuhuxi <mark.keckeis@gmail.com>
2014-02-23 09:48:10 -0700

    Added contact button and other changes

--- js/keckeisdotcom.js
+++ js/keckeisdotcom.js
@@ -8,13 +8,15 @@ $(document).ready(function()
     $("#banner-whatsthat").hide();
     $("#banner-shouldicare").hide();
     $("#banner-wecanhelp").hide();
-    $("#banner-aboutus").hide();
+    $("#banner-aboutme").hide();
+    $("#banner-contact").hide();
 
     $("#proposal-home").show();
     $("#proposal-whatsthat").hide();
     $("#proposal-shouldicare").hide();
     $("#proposal-wecanhelp").hide();
-    $("#proposal-aboutus").hide();
+    $("#proposal-aboutme").hide();
+    $("#proposal-contact").hide();
 
     //These are the scripts for the buttons
 
@@ -24,74 +26,109 @@ $(document).ready(function()
             $("#banner-whatsthat").hide();
             $("#banner-shouldicare").hide();
             $("#banner-wecanhelp").hide();
-            $("#banner-aboutus").hide();
+            $("#banner-aboutme").hide();
+            $("#banner-contact").hide();
 
             $("#proposal-home").show();
             $("#proposal-whatsthat").hide();
             $("#proposal-shouldicare").hide();
             $("#proposal-wecanhelp").hide();
-            $("#proposal-aboutus").hide();
+            $("#proposal-aboutme").hide();
+            $("#proposal-contact").hide();
             }
    
     })
+
     $("#lnk-what-button").on({
         click: function() {
             $("#banner-home").hide();
             $("#banner-whatsthat").show();
             $("#banner-shouldicare").hide();
             $("#banner-wecanhelp").hide();
-            $("#banner-aboutus").hide();
+            $("#banner-aboutme").hide();
+            $("#banner-contact").hide();
 
             $("#proposal-home").hide();
             $("#proposal-whatsthat").show();
             $("#proposal-shouldicare").hide();
             $("#proposal-wecanhelp").hide();
-            $("#proposal-aboutus").hide();
+            $("#proposal-aboutme").hide();
+            $("#proposal-contact").hide();
         }   
     })
+
     $("#lnk-care-button").on({
         click: function() {
             $("#banner-home").hide();
             $("#banner-whatsthat").hide();
             $("#banner-shouldicare").show();
             $("#banner-wecanhelp").hide();
-            $("#banner-aboutus").hide();
+            $("#banner-aboutme").hide();
+            $("#banner-contact").hide();
 
             $("#proposal-home").hide();
             $("#proposal-whatsthat").hide();
             $("#proposal-shouldicare").show();
             $("#proposal-wecanhelp").hide();
-            $("#proposal-aboutus").hide();
+            $("#proposal-aboutme").hide();
+            $("#proposal-contact").hide();
+
         } 
-    })  
+    })
+
     $("#lnk-help-button").on({
         click: function() {
             $("#banner-home").hide();
             $("#banner-whatsthat").hide();
             $("#banner-shouldicare").hide();
             $("#banner-wecanhelp").show();
-            $("#banner-aboutus").hide();
+            $("#banner-aboutme").hide();
+            $("#banner-contact").hide();
 
             $("#proposal-home").hide();
             $("#proposal-whatsthat").hide();
             $("#proposal-shouldicare").hide();
             $("#proposal-wecanhelp").show();
-            $("#proposal-aboutus").hide();
+            $("#proposal-aboutme").hide();
+            $("#proposal-contact").hide();
         }
     })
-    $("#lnk-about-button").on({
+
+    $("#lnk-aboutme-button").on({
         click: function() {
             $("#banner-home").hide();
             $("#banner-whatsthat").hide();
             $("#banner-shouldicare").hide();
             $("#banner-wecanhelp").hide();
-            $("#banner-aboutus").show();
+            $("#banner-aboutme").show();
+            $("#banner-contact").hide();
+
 
             $("#proposal-home").hide();
             $("#proposal-whatsthat").hide();
             $("#proposal-shouldicare").hide();
             $("#proposal-wecanhelp").hide();
-            $("#proposal-aboutus").show();
+            $("#proposal-aboutme").show();
+            $("#proposal-contact").hide();
+        }
+    })
+
+    $("#lnk-contact-button").on({
+        click: function() {
+            $("#banner-home").hide();
+            $("#banner-whatsthat").hide();
+            $("#banner-shouldicare").hide();
+            $("#banner-wecanhelp").hide();
+            $("#banner-aboutme").hide();
+            $("#banner-contact").show();
+
+
+            $("#proposal-home").hide();
+            $("#proposal-whatsthat").hide();
+            $("#proposal-shouldicare").hide();
+            $("#proposal-wecanhelp").hide();
+            $("#proposal-aboutme").hide();
+            $("#proposal-contact").show();
         }
     })
 
